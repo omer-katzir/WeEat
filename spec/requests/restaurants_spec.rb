7ec6f6dca7ffb8d577a7348f797d7bf064e0fad1
omer Katzir <omer.katzir@wework.com>
2019-05-05 16:35:49 +0300

    controller specs added- delete, put

--- spec/requests/restaurants_spec.rb
+++ spec/requests/restaurants_spec.rb
@@ -49,7 +49,6 @@ RSpec.describe 'Restaurants', type: :request do
   describe 'POST /restaurants' do
     before do
       post '/restaurants', params: FactoryBot.attributes_for(:restaurant)
-      puts "TEST:#{subject}"
     end
     it 'returns http success' do
       expect(response).to have_http_status(:created)
@@ -70,9 +69,47 @@ RSpec.describe 'Restaurants', type: :request do
     end
   end
 
+  describe 'PUT /restaurant/:id' do
+    subject { Restaurant.all.sample }
+
+    it 'returns https success' do
+      put restaurant_path(subject.id), params: FactoryBot.attributes_for(:restaurant)
+      expect(response).to have_http_status(:no_content)
+    end
+
+    it 'returns error when params are invalid' do
+      invalid_attr = FactoryBot.attributes_for(:restaurant, :invalid_record)
+      put restaurant_path(subject.id), params: invalid_attr
+      expect(response).to have_http_status(:unprocessable_entity)
+    end
+
+    it 'updates the record fields' do
+      new_rest = FactoryBot.build(:restaurant, id: subject.id)
+      put restaurant_path(subject.id), params: new_rest.attributes
+      rest = Restaurant.find(subject.id)
+      restaurant_matcher(rest, new_rest)
+    end
+  end
+
+  describe 'DELETE /restaurant/:id' do
+    subject { Restaurant.all.sample }
+
+    it 'returns http success' do
+      delete restaurant_path(subject.id)
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'fails to find the record in DB' do
+      delete restaurant_path(subject.id)
+      expect(Restaurant.find_by(id: subject.id)).to be_nil
+    end
+  end
+
   after(:all) do
     Restaurant.delete_all
   end
+
+  private
 
   def response_body
     JSON.parse(response.body)
@@ -88,6 +125,8 @@ RSpec.describe 'Restaurants', type: :request do
     float_matcher(rest_sample['longitude'], rest_ref.longitude)
     float_matcher(rest_sample['rating'], rest_ref.rating)
   end
+
+  private
 
   def float_matcher(f_ref, f_sample)
     f_delta = 0.0000000001
